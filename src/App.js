6d3b025f434bf7314cbe07967e6211cc7553f844
anthonyysz <anthony_szabo@hotmail.com>
2023-01-05 18:06:01 -0500

    fixing navbar

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/skills' element={<Skills/>} />
-      <Route path='/work' element={<Work/>} />
-      <Route path='/contact' element={<Contact/>} />
-    </Routes>
-    </>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
   );
 }
 
