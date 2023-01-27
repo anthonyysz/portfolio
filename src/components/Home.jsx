46848e637b8a62e14829e3adcee7ca3ce1308ce6
anthonyysz <anthony_szabo@hotmail.com>
2023-01-27 18:56:05 -0500

    changed home page description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#F8F0C6]'>Anthony Szabo</h1>
             <h2 className='text-4xl font-bold text-[#F8F0C6]'>I'm a Data Scientist</h2>
             <p className='text-l text-[#F8F0C6] py-4 max-w-[750px] text-[#F8F0C6]'>I'm a data scientist with a deep and expansive knowledge of statistics and I love digging into 
-                projects within that realm. Currently I am working to expand my knowledge of machine learning, especially in image recognition and natural language processing.</p>
+                projects within that realm. Currently I am working to expand my knowledge of machine learning, especially in neural networks.</p>
             <div>
                 <button className='text-[#F8F0C6] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#C4A484] hover:border-[#C4A484]'>
                   <Link to="work" smooth={true} duration={500}>
