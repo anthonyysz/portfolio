64724f98ac90f9554de6afa65fa0239030f166ac
anthonyysz <anthony_szabo@hotmail.com>
2023-02-20 11:48:37 -0500

    updated linkedin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
           <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-600'>
-              <a  className='flex justify-between items-center w-full text-[#F8F0C6]' href="https://www.linkedin.com/in/anthony-s-a3976622b/">
+              <a  className='flex justify-between items-center w-full text-[#F8F0C6]' href="https://www.linkedin.com/in/anthony-szabo-a3976622b/">
                 LinkedIn <FaLinkedin size={30} />
               </a>
             </li>
