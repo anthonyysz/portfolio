6aae01108c33c6362c061ca1dea976d783dcfb4a
anthonyysz <anthony_szabo@hotmail.com>
2026-08-09 22:17:19 -0400

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import logo from '../assets/logo.png';
-import resume from '../assets/resume.pdf'
+import resume from '../assets/anthony-szabo-resume.pdf'
 import '../style/Navbar.css'
 import { Link } from "react-router-dom";
 
@@ -59,7 +59,7 @@ const Navbar = () => {
               <a className='social-icon-text' href="mailto:anthony_szabo@hotmail.com">Email <HiOutlineMail size={30} /></a>
             </li>
             <li className='social-icon bg-[#565F69]'>
-              <a  className='social-icon-text' href={resume} download="resume.pdf">
+              <a  className='social-icon-text' href={resume} download="anthony-szabo-resume.pdf">
                 Resume <BsFillPersonLinesFill size={30} />
               </a>
             </li>
